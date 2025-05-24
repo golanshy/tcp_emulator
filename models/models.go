73ce21f7b2fb706ed04a196c109f767e5bdb27ea
Golan Shay <golanshy@gmail.com>
2025-05-24 12:21:23 +0100

    fix to device updated and created times

--- models/models.go
+++ models/models.go
@@ -2,15 +2,23 @@ package models
 
 import "time"
 
+type DataPoint struct {
+	DeviceId     string        `json:"device_id"`
+	Type         int           `json:"type"`
+	Location     LatLng        `json:"location"`
+	Measurements []Measurement `json:"measurements"`
+	CreatedAt    time.Time     `json:"created_at"`
+	UpdatedAt    time.Time     `json:"updated_at"`
+}
+
 type LatLng struct {
 	Lat float64 `json:"lat"`
 	Lng float64 `json:"lng"`
 }
 
-type DataPoint struct {
-	Id        string     `json:"id"`
-	Type      int        `json:"type"`
-	Location  LatLng     `json:"location"`
-	CreatedAt *time.Time `json:"createdAt"`
-	UpdatedAt *time.Time `json:"updatedAt"`
+type Measurement struct {
+	Index    int     `json:"index"`
+	SourceID string  `json:"source_id"`
+	Type     string  `json:"type"`
+	Value    float64 `json:"value"`
 }
--- utils/utils.go
+++ utils/utils.go
@@ -24,10 +24,37 @@ func CreateIndexMap(numberOfSources int, numberOfInstancesOfEachSource int, poin
 }
 
 func CreateIdsMap(numberOfSources int, numberOfInstancesOfEachSource int) map[int]string {
+
+	initialMap := make([]string, 0)
+	initialMap = append(initialMap, "ccf32c20-8475-4c71-9e63-80b15a506713")
+	initialMap = append(initialMap, "3b32105c-8252-4f3b-938a-04c828edc6e1")
+	initialMap = append(initialMap, "8e655452-90a3-4964-9d4e-633a115bef8d")
+	initialMap = append(initialMap, "7ba4700d-2df4-4a37-bd58-747cee104ff1")
+	initialMap = append(initialMap, "96337bad-ad44-4461-aa5c-023cb0d080ae")
+	initialMap = append(initialMap, "a7634b7e-1b51-4bb2-b909-9c8d65ef1d90")
+	initialMap = append(initialMap, "534ba91b-1c81-4508-a7e8-5e9944f87d24")
+	initialMap = append(initialMap, "2e812d48-2903-4c38-8893-383aeaccc457")
+	initialMap = append(initialMap, "5eda4ad7-0169-47af-bdd9-e30bcf263d14")
+	initialMap = append(initialMap, "a8ef2449-cce2-4140-8cc5-baeb73aade1e")
+	initialMap = append(initialMap, "03bc3576-2fec-4b54-aa81-f6a15264f979")
+	initialMap = append(initialMap, "0149c5eb-210f-4af9-bf3f-d7cbde27a115")
+	initialMap = append(initialMap, "feddc523-25ca-4ded-84ae-18c65cf10f63")
+	initialMap = append(initialMap, "73c620c2-01c1-440d-9f37-13e34c4a3c34")
+	initialMap = append(initialMap, "ad01409d-9ef7-4b73-9212-854c0ff0363e")
+	initialMap = append(initialMap, "8ba69b9d-d600-40bd-8396-2e235b05238a")
+	initialMap = append(initialMap, "5ad43b2d-5b5d-4150-bd3f-e463dbf4a13d")
+	initialMap = append(initialMap, "491d6a70-8948-4da8-b21e-a136f8958b54")
+	initialMap = append(initialMap, "4da07d03-0c96-4d1b-9aa4-7a33077d5d58")
+	initialMap = append(initialMap, "4fcc8959-3370-427a-af64-ea5205b5c988")
+
 	idMap := map[int]string{}
 	for i := 0; i < numberOfSources*numberOfInstancesOfEachSource; i++ {
-		id, _ := uuid.NewRandom()
-		idMap[i] = id.String()
+		if i < 20 {
+			idMap[i] = initialMap[i]
+		} else {
+			id, _ := uuid.NewRandom()
+			idMap[i] = id.String()
+		}
 	}
 	return idMap
 }
@@ -37,11 +64,11 @@ func CreateDataToSend(points *[]models.LatLng, dataPointId string, index int, t 
 
 	now := time.Now()
 	dataPoint := &models.DataPoint{
-		Id:        dataPointId,
+		DeviceId:  dataPointId,
 		Type:      t,
 		Location:  point,
-		CreatedAt: &now,
-		UpdatedAt: nil,
+		CreatedAt: now,
+		UpdatedAt: now,
 	}
 	jsonValue, err := json.Marshal(dataPoint)
 	if err != nil {
